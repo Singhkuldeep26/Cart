8fc4787c5272bce33f5ef42d40b17ea50e5e1b00
kuldeep singh <pssingh1145@gmail.com>
2023-02-13 11:57:52 +0530

    setState in depth

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,13 +10,69 @@ class CartItem extends React.Component{
             img:''
         }
         //this.increaseQuantity=this.increaseQuantity.bind(this);
+        this.testing();
+    }
+    testing () {
+        const promise=new Promise((resolve,reject) => {
+            setTimeout(() =>{
+                resolve('done');
+            },5000);
+        })
+        promise.then(() => {
+            //setState act like a synchronus call
+            this.setState({qty:this.state.qty+10});
+
+            this.setState({qty:this.state.qty+10});
+
+            this.setState({qty:this.state.qty+10});
+
+            console.log('state',this.state);
+        })
     }
     increaseQuantity = () => {
-        this.state.qty +=1;
-        console.log('this',this.state);
+        //this.state.qty +=1;
+        // console.log('this',this.state);
+
+        //setState from 1
+        // this.setState({
+        //     qty:this.state.qty +1
+        // });
+
+        // this.setState({
+        //     qty:this.state.qty +5
+        // });
+
+        //setState from 2 - if prevState required use this
+        // this.setState((prevState) =>{
+        //     return { 
+        //         qty:prevState.qty+1
+        //     }
+        // });
+
+        this.setState((prevState) =>{
+            return { 
+                qty:prevState.qty+1
+            }
+        },() =>{
+            console.log('this.state',this.state);
+        });
+        //console.log(this.state);
+    }
+    decreaseQuantity = () =>{
+        const {qty}=this.state;
+        if(qty===0){
+            return;
+        }
+        //setState from 2 - if prevState required use this
+        this.setState((prevState) =>{
+            return {
+                qty:prevState.qty-1
+            }
+        });
     }
     render(){
-        const{price,title,qty}=this.state
+        console.log('render');
+        const { price,title,qty } = this.state
         return (
             <div className='cart-item'>
                 <div className='left-block'>
@@ -39,6 +95,7 @@ class CartItem extends React.Component{
                      alt='decrease'
                      className='action-icons'
                      src='https://cdn-icons-png.flaticon.com/128/992/992683.png'
+                     onClick={this.decreaseQuantity}
                      />
                     <img
                      alt='delete'
